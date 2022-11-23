787b9d580a772edec73319395df8114c51ff793f
developerkunal <dawar.kunal703@gmail.com>
2022-11-24 00:28:49 +0530

    update

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,11 @@
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['ipfs.io'],
-},
-}
+    loader: "akamai",
+    path: "",
 
-module.exports = nextConfig
+  },
+};
